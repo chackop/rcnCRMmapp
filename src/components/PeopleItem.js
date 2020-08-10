62a028d37eed976969d4b50b778b82b1c28bdc7e
Chacko <chackop@gmail.com>
2020-08-10 23:12:21 +0100

    finished people list

--- src/components/PeopleItem.js
+++ src/components/PeopleItem.js
@@ -1,17 +1,53 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Flatlist} from 'react-native';
+import React from 'react';
+import {Text, View, StyleSheet, Image} from 'react-native';
 import {connect} from 'react-redux';
+import {getTheme} from 'react-native-material-kit';
+import Icon from 'react-native-vector-icons/EvilIcons';
+import * as actions from '../actions';
 
-const styles = StyleSheet.create({});
+const theme = getTheme();
 
-class PeopleItem extends Component {
-  render() {
-    return <View />;
-  }
-}
+const styles = StyleSheet.create({
+  card: {
+    marginTop: 20,
+  },
+  title: {
+    top: 20,
+    left: 80,
+    fontSize: 24,
+  },
+  image: {
+    height: 100,
+  },
+  action: {
+    backgroundColor: 'black',
+    color: 'white',
+  },
+  icon: {
+    position: 'absolute',
+    top: 15,
+    left: 0,
+    color: 'white',
+    backgroundColor: 'rgba(255,255,255,0)',
+  },
+});
 
-const mapStateToProps = (state) => {
-  return {people: state.people};
+const PeopleItem = (props) => {
+  return (
+    <View style={[theme.cardStyle, styles.card]}>
+      <Image
+        source={require('../images/background.jpg')}
+        style={[theme.cardImageStyle, styles.image]}
+      />
+      <Icon name={'user'} size={100} style={styles.icon} />
+      <Text style={[theme.cardTitleStyle, styles.title]}>
+        {props.people.firstName} {props.people.lastName}
+      </Text>
+      <Text style={[theme.cardActionStyle, styles.action]}>
+        {props.people.company}
+      </Text>
+    </View>
+  );
 };
 
-export default connect(mapStateToProps)(PeopleList);
+export default connect(null, actions)(PeopleItem);
